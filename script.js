4355e22c285745ddb7b1dc758b0c0da4fc81b7b7
mimanhsa-swarup <mimanhsaswarup@gmail.com>
2020-11-29 11:27:40 +0530

    feat: Translation done

--- script.js
+++ script.js
@@ -2,9 +2,33 @@ const txtinput = document.querySelector("#inpt-text");
 const button = document.querySelector("#inpt-btn");
 const outputarea = document.querySelector("#output-txt");
 
-function translateFunc() {
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-    outputarea.innerHTML = "hello " + txtinput.value
+function requiredURL(streng) {
+    return serverURL + "?" + "text=" + streng;
+
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function translateFunc() {
+    var inptText = txtinput.value
+
+
+
+    fetch(requiredURL(inptText))
+        .then(response => response.json())
+        .then(json => {
+
+
+            outputarea.innerText = json.contents.translated;
+        })
+        .catch(errorHandler)
+
 }
 
 button.addEventListener('click', translateFunc)
